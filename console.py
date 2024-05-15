c311a5696e735615ff399b559727f968123dcaea
enochCodes <henoksamson57@gmail.com>
2024-05-15 23:20:40 +0300

    define_console_command_fanctions

--- console.py
+++ console.py
@@ -12,6 +12,21 @@ class console(cmd.Cmd):
     prompt = '(hbnb)'
     file = None
 
+    def create():
+        """ create new object"""
+
+    def destroy():
+        """ delete objects """
+
+    def all():
+        """ List all objects or objects of a specific class """
+
+    def update():
+        """ update attributes of an object """
+
+    def quit():
+        """ Exit Console """
+
 
 if __name__ == '__main__':
     console().cmdloop()
