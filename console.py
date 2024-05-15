39bc642ea82945df7a7a8829063fd22ffb133efc
enochCodes <henoksamson57@gmail.com>
2024-05-16 02:31:29 +0300

    design_software_struchure_file_struchure

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import cmd
+import base_model
 """
     console shall to intract with,
     the sys console.py
@@ -12,6 +13,11 @@ class console(cmd.Cmd):
     prompt = '(hbnb)'
     file = None
 
+    def show():
+        """ to show or list """
+
+    def count():
+        """ to count objects """
     def create():
         """ create new object"""
 
