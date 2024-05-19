24cf13b45480e248b346975e0b6cef395a6044de
enochCodes <henoksamson57@gmail.com>
2024-05-19 23:58:16 +0300

    update_console

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 from models.base_model import BaseModel  # Commented out the problematic line
-from models.eangine.file_storage import FileStorage
+from eangine.file_storage import FileStorage
+
+print(file)
