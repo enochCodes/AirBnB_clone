39d7300e592404538ef24478e8e76363b30ea91b
enochCodes <henoksamson57@gmail.com>
2024-06-15 11:24:50 +0300

    done

---
+++ tests/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import unittest
+from models.base_mdel import BaseModel
+
+class TestFileStorage(unittest.TestCase):
+    def TestFileSize():
+
+
+if __name__ == '__main__':
+    unittest.main()
