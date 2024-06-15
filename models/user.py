5e4e50470c4986137555e666832e075d1a55d7ab
enochCodes <henoksamson57@gmail.com>
2024-06-15 18:08:22 +0300

    done

--- models/user.py
+++ models/user.py
@@ -9,4 +9,4 @@ class User(BaseModel):
     def __init__(self, first_name, last_name, passowrd):
         self.first_name = str(first_name)
         self.last_name = str(last_name)
-        self.password = str(password)
+        self.password = str(passowrd)
--- tests/test_models/__init__.py
+++ tests/test_models/__init__.py
@@ -2,5 +2,4 @@
 # tests/test_models/__init__.py
 from models.engine import file_storage
 # Import statements for the modules you want to test
-from .test_base_model import *
-from models import *
+from models.base_model import *
