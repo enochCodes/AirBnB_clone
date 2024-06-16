96db1790adab3726163008cddcc927bd28c10d68
enochCodes <henoksamson57@gmail.com>
2024-06-16 15:48:12 +0300

    done

--- models/base_model.py
+++ models/base_model.py
@@ -4,22 +4,14 @@
 from datetime import datetime
 import uuid
 
-
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
-            self.created_at = datetime.strptime(
-                    kwargs['created_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f'
-                    )
-            self.updated_at = datetime.strptime(
-                    kwargs['updated_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f'
-                    )
-                    
+            self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
         else:
             from models import storage
             self.id = str(uuid.uuid4())
@@ -41,5 +33,6 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,43 +2,35 @@
 """
     models/engine/file_storage.py
 """
-
 import json
-from datetime import datetime
 from models.base_model import BaseModel
-from models.base_model import *
-
 
 class FileStorage:
     """Handles storage of all models in the application."""
     __objects = {}
     __file_path = "file.json"
 
-    def __init__(self):
-        pass
-
     def all(self):
+        """Return all objects."""
         return self.__objects
 
     def new(self, obj):
-        key = obj.__class__.__name__ + "." + obj.id
+        """Add new object to storage dictionary."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes the objects to the JSON file."""
+        """Serialize objects to the JSON file."""
         with open(self.__file_path, 'w') as file:
-            json.dump(
-                {k: v.to_dict() for k, v in self.__objects.items()},
-                file
-            )
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, file)
 
     def reload(self):
-        """Deserialize the JSON file to __objects"""
+        """Deserialize the JSON file to __objects."""
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as f:
-                obj_dict = json.load(f)
-            for key, value in obj_dict.items():
-                cls = value.pop("__class__", None)
-                self.__objects[key] = eval(cls)(**value)
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    cls = value.pop("__class__", None)
+                    self.__objects[key] = globals()[cls](**value)
         except FileNotFoundError:
             pass
