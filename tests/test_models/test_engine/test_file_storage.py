c26f9bd0c1a2b6a67d375a73153c6435761d7b1e
enochCodes <henoksamson57@gmail.com>
2024-06-16 15:32:42 +0300

    done

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -51,18 +51,22 @@ class TestFileStorage(unittest.TestCase):
         object_key = f'BaseModel.{my_model.id}'
         self.assertIn(object_key, retrieved_objects)
         
-    def TestSaveRelode(self):
-        all_objs = storage.all()
-        print("-- Reloaded objects --")
-        for obj_id in all_objs.keys():
-            obj = all_objs[obj_id]
-            print(obj)
+    def test_Save(self):
         print("-- Create a new object --")
         my_model = BaseModel()
         my_model.name = "My_First_Model"
         my_model.my_number = 89
         my_model.save()
         print(my_model)
+    
+    def test_relod(self):
+        all_objs = storage.all()
+        print("-- Reloaded objects --")
+        for obj_id in all_objs.keys():
+            obj = all_objs[obj_id]
+            print(obj)
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
